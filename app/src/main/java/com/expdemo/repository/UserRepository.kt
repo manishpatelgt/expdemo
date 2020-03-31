e4568b2d99ff0cf517ddd632c21ebacd320ab470
Manish Patel <mdp3030@gmail.com>
2020-03-31 19:21:14 +0530

    - Handle error in older method of retrofit - testing left

--- app/src/main/java/com/expdemo/repository/UserRepository.kt
+++ app/src/main/java/com/expdemo/repository/UserRepository.kt
@@ -3,13 +3,17 @@ package com.expdemo.repository
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.expdemo.models.Post
+import com.expdemo.ui.retrofitdemo.ErrorCodes
+import com.expdemo.ui.retrofitdemo.Resource
 import com.expdemo.utils.extensions.safeApiCall
 import java.io.IOException
 import com.expdemo.utils.state.Result
 import kotlinx.coroutines.delay
 import retrofit2.Call
 import retrofit2.Callback
+import retrofit2.HttpException
 import retrofit2.Response
+import java.net.SocketTimeoutException
 
 class UserRepository(
     private val localDataSource: UserLocalDataSource,
@@ -50,11 +54,35 @@ class UserRepository(
     }
 
     suspend fun callPostApi(): Result<List<Post>> {
-        val response = retrofitApiService.getPosts()
+        return try {
+            val response = retrofitApiService.getPosts()
+            if (response.isSuccessful) return Result.Success(response.body()!!)
+            return Result.Error("Something went wrong. Please try again later!")
+        } catch (e: Exception) {
+            handleException(e)
+        }
+        /*val response = retrofitApiService.getPosts()
         if (response.isSuccessful) return Result.Success(response.body()!!)
-        return Result.Error(IOException("Something went wrong. Please try again later!"))
+        return Result.Error(IOException("Something went wrong. Please try again later!"))*/
     }
 
+    fun handleException(e: Exception): Result.Error {
+        return when (e) {
+            is HttpException -> Result.Error(getErrorMessage(e.code()))
+            is SocketTimeoutException -> Result.Error(getErrorMessage(ErrorCodes.SocketTimeOut.code))
+            else -> Result.Error(getErrorMessage(Int.MAX_VALUE))
+        }
+    }
+
+    private fun getErrorMessage(code: Int): String {
+        return when (code) {
+            ErrorCodes.SocketTimeOut.code -> "Timeout"
+            401 -> "Unauthorised"
+            404 -> "Not found"
+            else -> "Something went wrong"
+        }
+    }
+    
     suspend fun insert(post: Post) {
         localDataSource.postDao.insert(post)
     }
