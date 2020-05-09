ee0c3edd12ccb3f272cdc233da8c5e1a4dfc468b
Manish Patel <mdp3030@gmail.com>
2020-05-09 18:05:18 +0530

    - Short form of takePicture, pickimages and askpermission using Activity Results API

--- app/src/main/java/com/expdemo/ui/settingspanel/SettingsPanelActivity.kt
+++ app/src/main/java/com/expdemo/ui/settingspanel/SettingsPanelActivity.kt
@@ -13,18 +13,27 @@
 
 package com.expdemo.ui.settingspanel
 
+import android.app.Activity
 import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
 import android.provider.Settings
 import android.util.Log
 import android.widget.Toast
+import androidx.activity.invoke
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.expdemo.databinding.ActivitySettingsPanelBinding
 import com.expdemo.utils.constants.Constants.ANY_WEBSITE_URL
 import com.expdemo.utils.extensions.isAtLeastAndroid9
 import com.expdemo.utils.extensions.isNetworkAvailable
 import com.google.android.material.snackbar.Snackbar
-
+//StartActivityOnResult()
+//https://pluu.github.io/blog/android/2020/03/24/activity-result/
+//https://wajahatkarim.com/2020/05/activity-results-api-onactivityresult/
+//https://developer.android.com/training/basics/intents/result
 /**
  * Created by Manish Patel on 5/9/2020.
  */
@@ -42,6 +51,11 @@ class SettingsPanelActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         loadWebPage()
+
+        binding.button.setOnClickListener {
+            pickImages("image/*")     // We want images, so we set the mimeType as "image/*"
+           // askForPermission(android.Manifest.permission.CAMERA)
+        }
     }
 
     private fun loadWebPage() {
@@ -58,8 +72,55 @@ class SettingsPanelActivity : AppCompatActivity() {
             .show()
     }
 
+    val askForPermission =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted  ->
+            if (isGranted) {
+                //start camera
+                var imageUri: Uri? = null
+                takePicture(imageUri)
+            }
+        }
+
+    val takePicture =
+        registerForActivityResult(ActivityResultContracts.TakePicture()) { bitmap: Bitmap? ->
+            // Do something with the Bitmap, if present
+            bitmap?.let {
+                binding.imgView.setImageBitmap(it)
+            }
+        }
+
+    // Pick Images Contract - Normally this is for all kind of files.
+    private val pickImages = registerForActivityResult(ActivityResultContracts.GetContent()) {uri ->
+        uri?.let {uri ->
+            binding.imgView.setImageURI(uri)
+        }
+    }
+
+    val startForResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                //val intent = result.data
+                // Handle the Intent
+                loadWebPage()
+            }
+        }
+
     private fun displaySettingsPanel() {
         try {
+            var intent = if (isAtLeastAndroid9()) {
+                Intent(Settings.Panel.ACTION_INTERNET_CONNECTIVITY)
+            } else {
+                Intent(Settings.ACTION_WIFI_SETTINGS)
+            }
+            startForResult(intent)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            intent = Intent(Settings.ACTION_WIFI_SETTINGS)
+            startForResult(intent)
+        }
+
+        // Older way to handle startActivityForResult()
+        /*try {
             var intent = if (isAtLeastAndroid9()) {
                 Intent(Settings.Panel.ACTION_INTERNET_CONNECTIVITY)
             } else {
@@ -70,7 +131,7 @@ class SettingsPanelActivity : AppCompatActivity() {
             e.printStackTrace()
             intent = Intent(Settings.ACTION_WIFI_SETTINGS)
             startActivityForResult(intent, REQUEST_CODE)
-        }
+        }*/
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
