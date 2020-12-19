92630d6903a0152787a21e7de0830c6192f9106f
Manish Patel <mdp3030@gmail.com>
2020-12-19 20:57:15 +0530

    - New branch for testing view model

---
+++ app/src/test/java/com/androidtestingdemo/ui/ShoppingViewModelTest.kt
@@ -0,0 +1,87 @@
+package com.androidtestingdemo.ui
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.androidtestingdemo.MainCoroutineRule
+import com.androidtestingdemo.getOrAwaitValueTest
+import com.androidtestingdemo.other.Constants
+import com.androidtestingdemo.other.Status
+import com.androidtestingdemo.repositories.FakeShoppingRepository
+import org.junit.Before
+import org.junit.Test
+import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Rule
+
+@ExperimentalCoroutinesApi
+class ShoppingViewModelTest {
+
+    @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    val mainCoroutineRule = MainCoroutineRule()
+
+    private lateinit var viewModel: ShoppingViewModel
+
+    @Before
+    fun setup() {
+        viewModel = ShoppingViewModel(FakeShoppingRepository())
+    }
+
+    @Test
+    fun `insert shopping item with empty field, returns error`() {
+        viewModel.insertShoppingItem("Banana", "", "3.0")
+
+        val value = viewModel.insertShoppingItemStatus.getOrAwaitValueTest()
+
+        assertThat(value.getContentIfNotHandled()?.status).isEqualTo(Status.ERROR)
+    }
+
+    @Test
+    fun `insert shopping item with too long name, returns error`() {
+        val string = buildString {
+            for (i in 1..Constants.MAX_NAME_LENGTH + 1) {
+                append(i)
+            }
+        }
+        viewModel.insertShoppingItem(string, "5", "3.0")
+
+        val value = viewModel.insertShoppingItemStatus.getOrAwaitValueTest()
+
+        assertThat(value.getContentIfNotHandled()?.status).isEqualTo(Status.ERROR)
+    }
+
+    @Test
+    fun `insert shopping item with too long price, returns error`() {
+        val string = buildString {
+            for (i in 1..Constants.MAX_PRICE_LENGTH + 1) {
+                append(i)
+            }
+        }
+        viewModel.insertShoppingItem("Banana", "5", string)
+
+        val value = viewModel.insertShoppingItemStatus.getOrAwaitValueTest()
+
+        assertThat(value.getContentIfNotHandled()?.status).isEqualTo(Status.ERROR)
+    }
+
+    @Test
+    fun `insert shopping item with too high amount, returns error`() {
+
+        viewModel.insertShoppingItem("Banana", "9999999999999999999", "3.0")
+
+        val value = viewModel.insertShoppingItemStatus.getOrAwaitValueTest()
+
+        assertThat(value.getContentIfNotHandled()?.status).isEqualTo(Status.ERROR)
+    }
+
+    @Test
+    fun `insert shopping item with valid input, returns success`() {
+
+        viewModel.insertShoppingItem("Banana", "5", "3.0")
+
+        val value = viewModel.insertShoppingItemStatus.getOrAwaitValueTest()
+
+        assertThat(value.getContentIfNotHandled()?.status).isEqualTo(Status.SUCCESS)
+    }
+}
